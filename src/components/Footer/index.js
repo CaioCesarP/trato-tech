cb0ce1f3fccbb1d0ff89b18b696783db20abde5e
caio.cp <caio.cesar@itarget.com.br>
2022-10-18 08:32:12 -0300

    :sparkles: feat: redes sociais com direcionamento.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,15 +4,16 @@ import { FaMailBulk, FaGithub, FaLinkedin } from "react-icons/fa";
 const iconeProps = {
   color: "white",
   size: 24,
+  cursor: "pointer",
 };
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div>
-        <FaMailBulk {...iconeProps} />
-        <FaGithub {...iconeProps} />
-        <FaLinkedin {...iconeProps} />
+        <FaMailBulk onClick={() => window.open("mailto:caiocesarworks@gmail.com")} {...iconeProps} />
+        <FaGithub onClick={() => window.open("https://github.com/CaioCesarP/trato-tech")} {...iconeProps} />
+        <FaLinkedin onClick={() => window.open("https://www.linkedin.com/in/profile-caio-c%C3%A9sar-link/")} {...iconeProps} />
       </div>
       <span>Desenvolvido por Caio César Ponte.</span>
     </footer>
